ce4d2bfc4bd5eed305e3eae757aa8ca49d919626
ejportil <cristian.mira@sofka.com.co>
2020-10-04 15:30:40 -0500

    Implementation TestRunner GoogleSearch

---
+++ src/test/java/GoogleSearchTestRunner.java
@@ -0,0 +1,12 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/main/resources/features",
+        glue = ""
+)
+public class GoogleSearchTestRunner {
+
+}
